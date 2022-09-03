00641037c45991051f4a680ac66410cb7fc06b7f
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-03 15:03:37 +0800

    KotlinProgram4

---
+++ src/main/kotlin/KotlinProgram4.kt
@@ -0,0 +1,26 @@
+fun main () {
+
+    var number : Int = 10
+
+    if(number < 10) {
+        println("Hello)")
+    }
+
+    var grades : Int = 70
+    if (grades >= 75) {
+        println("Passed")
+    }
+
+
+    //range
+    var idNumber : Int = 1
+    //true                 //true
+    if((idNumber >= 1) && (idNumber<=3)){
+        println("Valid ID number")
+        println("Success")
+    }
+
+
+    println("Line Ends")
+
+}
