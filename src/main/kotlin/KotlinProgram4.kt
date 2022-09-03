37db39ef30596f7ce6c9543435d4816be891ff13
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-03 15:26:10 +0800

    Kotlin Program 4 2nd

--- src/main/kotlin/KotlinProgram4.kt
+++ src/main/kotlin/KotlinProgram4.kt
@@ -6,19 +6,31 @@ fun main () {
         println("Hello)")
     }
 
-    var grades : Int = 70
-    if (grades >= 75) {
-        println("Passed")
+    var grades : Int = 74
+
+    if((grades >=96) && (grades <=100)) {
+        println("1.00")
+    }
+    else if((grades >=86) && (grades <=95)){
+        println("1.75")
+    }
+    else if ((grades >= 76) && (grades <=85)) {
+        println("2.25")
+    }
+    else if(grades < 75){
+        println("5.0")
+    }else{
+        println("Invalid grade!")
     }
 
 
-    //range
-    var idNumber : Int = 1
-    //true                 //true
-    if((idNumber >= 1) && (idNumber<=3)){
-        println("Valid ID number")
-        println("Success")
-    }
+//    //range
+//    var idNumber : Int = 1
+//    //true                 //true
+//    if((idNumber >= 1) && (idNumber<=3)){
+//        println("Valid ID number")
+//        println("Success")
+//    }
 
 
     println("Line Ends")
