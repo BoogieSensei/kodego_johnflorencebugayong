be522904348b88dcef9efda7cfe22863658a6913
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-03 11:18:01 +0800

    Practice Activity 2

---
+++ src/main/kotlin/practice_activities/PracticeActivity2.kt
@@ -0,0 +1,15 @@
+package practice_activities
+
+
+fun main () {
+    var mathGrade : Double = 78.00
+    var englishGrade : Double = 81.00
+    var filipinoGrade : Double = 98.00
+
+    var averageGrade : Double = (mathGrade + englishGrade + filipinoGrade) / 3
+
+    println("The average grade is $averageGrade")
+
+
+
+}
