c65386d761dee1e1bab05dd85636f0f848eb27bf
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-03 15:36:03 +0800

    Kotlin Program 5 1st commit

---
+++ src/main/kotlin/KotlinProgram5.kt
@@ -0,0 +1,15 @@
+fun main (){
+
+    var itemNumber: Int = 1
+
+    when (itemNumber){
+        1 -> print("clothes")
+        2 -> print("food")
+        3 -> print("toiletries")
+        else -> print ("invalid item number")
+
+    }
+
+
+
+}
