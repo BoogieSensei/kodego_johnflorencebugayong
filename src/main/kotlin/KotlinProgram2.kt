769b35291fae96d681a448f0f3f3ee05918f3e0e
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-03 09:50:02 +0800

    innitial commit for KotlinProgram2.kt

---
+++ src/main/kotlin/KotlinProgram2.kt
@@ -0,0 +1,28 @@
+fun main(){
+    var string1 : String = "This is my String"
+    var string2 : String = "This is my String too"
+    //String character
+
+    println(string1[11])
+    //string length
+    println(string1. length)
+    println(string2. length)
+    //string equality
+    println(string1 == string2)
+    //string uppercase/lowercase
+    println(string2. uppercase())
+    //string uppercase/lowercase
+    println(string2.lowercase())
+    //startsWith
+    println(string2.startsWith("tha"))
+    //endsWith
+    println(string2.endsWith("too"))
+    //concatenate
+    println(string1+" "+string2)
+    //substring
+    println(string1.substring(6,10))
+    //replace
+    println(string1.replace("this","that"))
+
+
+}
