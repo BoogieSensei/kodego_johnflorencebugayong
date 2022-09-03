f82516a5bdfce79bc6ae40613d731c379499b826
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-03 11:10:13 +0800

    PracticeActivity 1 redo

---
+++ src/main/kotlin/practice_activities/PracticeActivity1.kt
@@ -0,0 +1,12 @@
+package practice_activities
+
+
+fun main (){
+    var firstName : String = "John FLorence"
+    var middleName: String = "Endraca"
+    var surName: String = "Bugayong"
+
+    println("My name is $firstName $middleName $surName")
+
+
+}
