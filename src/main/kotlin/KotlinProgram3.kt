f723645df2b5fdfa5ba20aea196ebb455996a0ff
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-03 10:30:08 +0800

    First Commit for KotlinProgram3.kt

---
+++ src/main/kotlin/KotlinProgram3.kt
@@ -0,0 +1,23 @@
+fun main(){
+    var number1 : Int = 21
+    var number2 : Int = 21
+    var answer : Int = number1 % number2
+
+    println("Answer is $answer")
+
+    //complex operations
+    var complexFormula: Int = (2+10)+(10/2)
+    println(complexFormula)
+
+    //comparison operations
+
+    var expression : Boolean = !(number1 < number2)
+    println(expression)
+
+    //increment operators
+    var incrementNum : Int = --number1
+    println(incrementNum)
+
+
+
+}
