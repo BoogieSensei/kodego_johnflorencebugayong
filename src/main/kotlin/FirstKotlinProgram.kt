3e1dfc37c780fc7fb552c49dd98730fa324d5289
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-03 09:11:53 +0800

    Eight Commit

--- src/main/kotlin/FirstKotlinProgram.kt
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -5,18 +5,17 @@ fun main (){
     Date Created: 9/1/2022
     Filename: FirstKotlinProgram.kt
      */
-    println("Hello World")
-    println("Hello World!!!!")
-    println("Hello World!!!!!!!!!!")
-    println("I am John Florence E. Bugayong")
-    println("Former Top 6 Saber in Philippines")
 
-    println("Trying to figure out how to code!!!!!")
-    // Always remember to lessen comments.
-    println("Finished practicing what was discussed yesterday.")
-    println("Ready to start the 3rd day of the Bootcamp.")
-    println("Hello World4")
-    println("Hello World5")
-    println("Hello World6")
+    println("Enter number 1:")
+    var number1 : Int = readln().toInt()
+
+    println("Enter Number 2:")
+    var number2 : Int = readln().toInt()
+
+    var sum : Int = number1 + number2
+
+    println("The sum is $sum")
+
+
 
 }
