f61e3b0b5c69db65e476394c95ab3e9cb2c7a6de
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-05 00:21:04 +0800

    Exercise1E

---
+++ src/main/kotlin/exercises/Exercise1E.kt
@@ -0,0 +1,25 @@
+package exercises
+
+
+fun main (){
+
+    /*
+    Author: John Florence E. Bugayong
+    Date Created: 9/4/2022
+    Filename: Exercise1E.kt
+     */
+
+
+
+    println("Please type in your name:")
+    var clientName1 : String = readln().toString()
+
+    println("Please type in your current address")
+    var clientAddress1 : String = readln().toString()
+
+    println("You are $clientName1. Currently living in $clientAddress1")
+
+
+
+
+}
