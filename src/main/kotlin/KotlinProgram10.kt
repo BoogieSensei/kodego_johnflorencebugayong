553b0bc5fbea4592b859a007b9d49310d4d34a82
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-05 20:15:55 +0800

    KotlinProgram10

---
+++ src/main/kotlin/KotlinProgram10.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    //Loop within a loop
+    var ctr1 : Int = 1
+    while(ctr1 <=5){
+        var ctr2 : Int = 1
+        while (ctr2 <=5){
+            print("@")//@@@@@
+            ctr2++
+        }
+        println() //empty next line
+        ctr1++
+    }
+
+
+}
