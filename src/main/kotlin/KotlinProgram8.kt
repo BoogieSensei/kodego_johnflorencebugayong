e78e205478a6feb44ff34caeebff10b1225bc536
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-05 19:35:07 +0800

    KotlinProgram8.kt

---
+++ src/main/kotlin/KotlinProgram8.kt
@@ -0,0 +1,29 @@
+fun main(){
+    //while loop
+    //initialization
+
+    var ctr : Int = 1
+
+    //condition
+    while(ctr <= 5){
+        println("Hello World $ctr")
+        //update
+        ctr++
+    }
+
+
+
+    //    ctr++
+    //  println(ctr)
+
+
+
+
+
+
+
+
+
+
+
+}
