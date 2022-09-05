f04013de8ee04d006a9de18397e49360bbfd9a10
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-05 19:54:50 +0800

    KotlinProgram9

---
+++ src/main/kotlin/KotlinProgram9.kt
@@ -0,0 +1,14 @@
+fun main(){
+    //do while
+    //intialization
+    var ctr : Int = 1
+
+    do{
+        println("Hello World")
+        ctr++ //update
+    }while(ctr <=5)//condition
+        if(ctr == 3){
+            println("Hello World $ctr")
+        }
+
+}
