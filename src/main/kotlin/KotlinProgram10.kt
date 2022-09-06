f8675ff049f4ba2b1a806beccc51d127367ac769
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-06 19:18:55 +0800

    KotlinProgram10 update

--- src/main/kotlin/KotlinProgram10.kt
+++ src/main/kotlin/KotlinProgram10.kt
@@ -1,13 +1,15 @@
 fun main(){
 
-    //Loop within a loop
+    //Loop within a loop or Nested Loop
     var ctr1 : Int = 1
-    while(ctr1 <=5){
+    while(ctr1 <=3){
+        //start of inner loop
         var ctr2 : Int = 1
-        while (ctr2 <=5){
+        while (ctr2 <=3){
             print("@")//@@@@@
             ctr2++
         }
+        //end of inner loop
         println() //empty next line
         ctr1++
     }
