b246b9e23207d64a878efda2e3f9c4c4333b6f8c
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-06 19:16:05 +0800

    KotlinProgram10 update

--- src/main/kotlin/exercises/Exercise1G.kt
+++ src/main/kotlin/exercises/Exercise1G.kt
@@ -8,17 +8,22 @@ fun main (){
     Filename: Exercise1G.kt
      */
 
-    println("Please enter your first Number:")
-    var clientNumber1 : Int = readln().toInt()
+//    println("Please enter your first Number:")
+//    var clientNumber1 : Int = readln().toInt()
+//
+//    println("Please enter your second Number:")
+//    var clientNumber2 : Int = readln().toInt()
+//
+//    if (clientNumber2 <= 1000000){
+//        println("You have entered $clientNumber1 and $clientNumber2")
+//    }
 
-    println("Please enter your second Number:")
-    var clientNumber2 : Int = readln().toInt()
+    var ctr : Int = 1
+    while (ctr <=10){
+        println("Hello")
+        ctr++
+    }
 
 
-
-
-//       val arr = arrayOf()
-//        arr.sort()
-//        for (x in arr) print("$x  ")
 
 }
