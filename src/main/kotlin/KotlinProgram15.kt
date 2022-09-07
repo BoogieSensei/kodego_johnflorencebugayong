6481763cb0e668e598dc06053542c91d30d05a84
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-07 20:33:11 +0800

    KotlinProgram15

---
+++ src/main/kotlin/KotlinProgram15.kt
@@ -0,0 +1,24 @@
+fun main(){
+    var num = arrayOf(
+        //                  0     1       2
+        arrayOf<String>("Peter","Paul","Mary"),         //0
+        arrayOf<String>("John","Mark","Matthew"),       //1
+        arrayOf<String>("James","Luke","Zachary"),      //2
+    )
+//    println(num[2][1])
+//    println(num[1][2])
+
+
+    var row : Int = 0
+    while (row <3){
+        var column : Int = 0
+        while(column <3){
+            print(" ${num[row][column]} ")
+            column++
+
+        }
+        println()
+        row++
+
+    }
+}
