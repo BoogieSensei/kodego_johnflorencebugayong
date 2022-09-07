5ffe6ad9b5a58d5a908f6a76a96cabe05e57b8c1
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-07 20:21:08 +0800

    KotlinProgram14

---
+++ src/main/kotlin/KotlinProgram14.kt
@@ -0,0 +1,27 @@
+fun main(){
+//    var names = arrayOf<String>("John","Paul","Mark","Ringo","George")
+//    var ctr : Int = 0
+//    while(ctr < names.size){
+//        println(names[ctr])
+//
+//        ctr++
+//
+//
+//    }
+
+    var names = arrayOf<String>("John","Paul","Mark","Ringo","George")
+    var ctr : Int = 0
+    while (ctr < names.size){
+        println("Enter new value for index $ctr")
+        names[ctr] = readln()
+
+        ctr++
+    }
+    var ctr2 : Int = 0
+    while(ctr2 < names.size){
+        println(names[ctr2])
+
+        ctr2 ++
+    }
+
+}
