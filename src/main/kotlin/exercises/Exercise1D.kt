1bc9d838a8dfcc4bfe44208dc2261de53e70a337
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-07 16:12:03 +0800

    Exercise1D updated

--- src/main/kotlin/exercises/Exercise1D.kt
+++ src/main/kotlin/exercises/Exercise1D.kt
@@ -1,6 +1,4 @@
-package exercises
-
-fun main(){
+fun main () {
 
     /*
     Author: John Florence E. Bugayong
@@ -9,36 +7,37 @@ fun main(){
      */
 
     //Start of the program
-    println("please enter amount")
-    var amount1 : Double = readln().toDouble()
+
+    println("Please Enter Amount:")
+    var num1: Int = readln().toInt()
+    var condition: Boolean = true
+
+    while (condition) {
+        println("Do you want to add another amount? YES or NO?")
+        var input2: String = readln().toString()
 
 
-    //Will the client add any amount?
-    if ((amount1 >=1) && (amount1 <= 1000000000000)){
-     println("The amount your entered is $amount1.")
+        if (input2 == "YES") {
+            print("Enter Amount:")
+            var input: Int = readln().toInt()
+            num1 = num1 + input
+            println()
+            println("The total amount you entered is $num1")
+
+        } else if (input2 == "NO") {
+            println("The total value you entered is $num1")
+
+            println("Divide the total value by how many?")
+            var divisor: Int = readln().toInt()
+
+            var totalAmount: Int = num1 / divisor
+            println("The total amount is $totalAmount")
+
+            condition = false
+        }
+
     }
-    println("Do you want to add another amount? Please answer either YES or NO")
-    var amount2: String = readln().toString()
-
-    //The client will add amount
-    if(amount2 == "YES") {
-            println("Please enter additional amount.")
-    var amount3 : Double = readln().toDouble()
-        var totalAmount1 : Double = amount1 + amount3
-        println("The total amount you enter is $totalAmount1")
-        println("Divide the value by how many?")
-        var divisor1 : Int = readln().toInt()
-        var grandTotal1 : Double = totalAmount1 / divisor1
-        println("Your total amount is $grandTotal1")
-
-    //The client will NOT add amount
-    }else if(amount2 == "NO"){
-        println("Divide the value by how many?")
-        var divisor2 : Int = readln().toInt()
-        var grandTotal2 : Double = amount1 / divisor2
-        println("Your total amount is $grandTotal2")
-    }
-
-    println("Thank you so much for your transaction. We hope to business with you again.")
-
+    println()
+    println("Thank you so much for you transaction. We hope to see you again.")
 }
+
