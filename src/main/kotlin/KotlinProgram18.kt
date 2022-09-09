4c811fcbee1a4d49bd8a328bc6b6bd6e91212c5e
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-09 20:27:03 +0800

    KotlinProgram18

--- src/main/kotlin/KotlinProgram18.kt
+++ src/main/kotlin/KotlinProgram18.kt
@@ -1,6 +1,26 @@
 fun main (){
+     // declaring maps
     var map = mapOf(1 to "Peter", 2 to "Paul", 3 to "Mary")
+    var map2 = mutableMapOf<Int,String>()
+    map2.put(2,"John")
+    map2.remove(2)
+
+    println(map2)
+
     println(map)
+    //getting value from map
+    println(map[1])
+    println(map[3])
+    println(map.get(2))
+
+    //map traversing keys
+    for (key in map.keys){
+        print(key)
+    }
+    for (value in map.values){
+        print(value)
+    }
+
 
 
 }
