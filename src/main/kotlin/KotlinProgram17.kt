d04fd71b9929a86a871486dfbd0f22bd93057e38
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-09 19:58:40 +0800

    KotlinProgram17

--- src/main/kotlin/KotlinProgram17.kt
+++ src/main/kotlin/KotlinProgram17.kt
@@ -1,6 +1,17 @@
 fun main (){
 
     var mySet = setOf(2,3,6,1,2,2,2,2,2,2)
+    //mutable set
+    var mySet2 = mutableSetOf<Int>()
+    mySet2.add(5)
+    mySet2.add(7)
+    mySet2.add(8)
+    mySet2.add(8)
+    mySet2.remove(8)
+
+    for (data in mySet2){
+        print(data)
+    }
 
 //    println(mySet.count())
 //    println(mySet.sum())
@@ -10,13 +21,13 @@ fun main (){
 //        print(data)
 //    }
 
-    var setA = setOf(2,5,7,8)
-    var setB = setOf(1,3,5,7)
-
-
-    println(setA.union(setB))
-    println(setA.intersect(setB))
-    println(setA.minus(setB))
-    println(setB.minus(setA))
+//    var setA = setOf(2,5,7,8)
+//    var setB = setOf(1,3,5,7)
+//
+//
+//    println(setA.union(setB))
+//    println(setA.intersect(setB))
+//    println(setA.minus(setB))
+//    println(setB.minus(setA))
 
 }
