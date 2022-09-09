1bbbff17eaea01fa67916e02b79e4e90a5b8412c
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-09 20:10:11 +0800

    KotlinProgram17

--- src/main/kotlin/KotlinProgram17.kt
+++ src/main/kotlin/KotlinProgram17.kt
@@ -13,21 +13,23 @@ fun main (){
         print(data)
     }
 
-//    println(mySet.count())
-//    println(mySet.sum())
-//    println(mySet.average())
-//
-//    for (data in mySet){
-//        print(data)
-//    }
+    println(mySet.count())
+    println(mySet.sum())
+    println(mySet.average())
 
-//    var setA = setOf(2,5,7,8)
-//    var setB = setOf(1,3,5,7)
-//
-//
-//    println(setA.union(setB))
-//    println(setA.intersect(setB))
-//    println(setA.minus(setB))
-//    println(setB.minus(setA))
+    for (data in mySet){
+        print(data)
+    }
+
+    var setA = setOf(2,5,7,8)
+    var setB = setOf(1,3,5,7)
+
+    //union
+    println(setA.union(setB))
+    //intersection
+    println(setA.intersect(setB))
+    //difference
+    println(setA.minus(setB))
+    println(setB.minus(setA))
 
 }
