8b1ed09b85128c5b4440eb30e4fc8483ef5c96f0
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-10 11:27:59 +0800

    KotlinProgram21

---
+++ src/main/kotlin/KotlinProgram21.kt
@@ -0,0 +1,25 @@
+fun main(){
+
+    //instantiation - process of creating an object
+    var myObject = MyClass("Hello World")
+    var myObject2 = MyClass ("Hi World")
+}
+
+
+class MyClass(value : String){
+    init{
+        println(value)
+    }
+
+
+}
+    /*
+    class -> objects
+    object -> variable
+           -> functions(process)
+    uppercase -> first letter
+    examples.
+    String
+    Int
+
+     */
