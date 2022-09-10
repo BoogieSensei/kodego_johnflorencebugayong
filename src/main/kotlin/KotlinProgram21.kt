51e7ab0ea9666cd6df50b91ab8c35d8153e91a28
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-10 11:38:06 +0800

    KotlinProgram21

--- src/main/kotlin/KotlinProgram21.kt
+++ src/main/kotlin/KotlinProgram21.kt
@@ -2,13 +2,18 @@ fun main(){
 
     //instantiation - process of creating an object
     var myObject = MyClass("Hello World")
-    var myObject2 = MyClass ("Hi World")
+    var myGenericObject = MyGenericClass(true)
 }
-
+//Generics
+class MyGenericClass<T>(value:T){
+    init{
+        println(value.toString().length)
+    }
+}
 
 class MyClass(value : String){
     init{
-        println(value)
+        println(value.length)
     }
 
 
