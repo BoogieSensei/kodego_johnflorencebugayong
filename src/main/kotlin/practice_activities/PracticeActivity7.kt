a68e34795c82d9730836291049fdcd1cfc7c0225
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-12 01:47:52 +0800

    PracticeActivity7

---
+++ src/main/kotlin/practice_activities/PracticeActivity7.kt
@@ -0,0 +1,23 @@
+package practice_activities
+
+fun main(){
+    var length : Double = 4.5
+    var width : Double = 2.0
+
+    var perimeter: Double = calculatePerimeter(length,width)
+    println(perimeter)
+    var area: Double = calculateArea(length,width)
+    println(area)
+
+
+}
+
+fun calculatePerimeter( length: Double, width: Double):Double{
+    return (length + width)*2
+
+}
+
+fun calculateArea(length: Double, width:Double):Double{
+    return length * width
+
+}
