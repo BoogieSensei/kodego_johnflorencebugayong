56329309d279f59d8800f9f57d118558c30821be
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-19 12:09:30 +0800

    PracticeActivity10

--- src/main/kotlin/practice_activities/PracticeActivity10.kt
+++ src/main/kotlin/practice_activities/PracticeActivity10.kt
@@ -4,7 +4,7 @@ fun main(){
 
     var phone = SmartPhone(20.0, 50)
     phone.buyLoad(50.0)
-    println(phone.load)
+    println("Your current load is ${phone.load}")
 
 
 
@@ -16,6 +16,7 @@ class SmartPhone(var load: Double, var batteryPercentage:Int) {
     init {
         println("Object Created!")
         println("Initial value of load is $load and battery percentage is $batteryPercentage%")
+        println("***************")
     }
 
     //set parameter for functions
@@ -27,34 +28,32 @@ class SmartPhone(var load: Double, var batteryPercentage:Int) {
      */
 
     fun buyLoad(amount: Double): Double {
-
         if (amount > 0) {
+
+
             var newAmount = load + amount
-            this.load = newAmount
+            load = newAmount
             println("You have purchased $amount pesos load.")
             var condition = true
             while (condition) {
                 println("Your current load is $load pesos")
                 println("Do you want to add more load? YES or NO")
                 var input = readln().uppercase()
-                var condition2 = true
-                while (condition2) {
-                    if (input == "YES") {
-                        println("please enter another amount")
-                        var newAmount2: Double = readln().toDouble()
-                        load = load + newAmount2
-                    } else if (input == "NO") {
-                        condition2 = false
 
-                    } else {
-                        println("You have entered an invalid amount.")
-                        condition2 = false
-                    }
+                if (input == "YES") {
+                    println("please enter another amount:")
+                    var newAmount2: Double = readln().toDouble()
+                    load = load + newAmount2
+                } else if (input == "NO") {
+                    condition = false
+
+                } else {
+                    println("You have entered an invalid answer.")
                 }
-                condition = false
+
             }
         }else{
-            println("You have entered an invalid amount. Please try again. Have a nice day")
+            println("You have entered and invalid amount. Please try again.")
         }
         return load
     }
