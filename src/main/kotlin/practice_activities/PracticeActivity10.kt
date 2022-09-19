cc96bcae8ba85645545b3ef70947da5d3fa2af51
John Florence E. Bugayong <bugayongjohn06031994@gmail.com>
2022-09-19 16:44:18 +0800

    PracticeActivity10

--- src/main/kotlin/practice_activities/PracticeActivity10.kt
+++ src/main/kotlin/practice_activities/PracticeActivity10.kt
@@ -2,12 +2,20 @@ package practice_activities
 
 fun main(){
 
-    var phone = SmartPhone(20.0, 50)
+    var phone = SmartPhone(60.0, 50)
 //    phone.buyLoad(50.0)
 //    println("Your current load is ${phone.load}")
 
 //    phone.chargeBattery(0)
 //    println("The current battery percentage is ${phone.batteryPercentage}%")
+
+//    phone.sendText("Hello World!")
+
+
+//    phone.checkBalance()
+
+    phone.call(8)
+
 }
 
 
@@ -17,13 +25,6 @@ class SmartPhone(var load: Double, var batteryPercentage:Int) {
         println("Initial value of load is $load and battery percentage is $batteryPercentage%")
         println("***************")
     }
-
-    //set parameter for functions
-    /*  chargeBattery(percent) // does not accept 0 or negative values, should not exceed 100%
-        sendText(message) // display message sent, load will decrement
-        checkBalance() // display current load, if zero, display "Check operator services"
-        call(minutes) // 8 load per minute, will not push through if no sufficient load
-     */
 
     //Buying load function
     fun buyLoad(amount: Double): Double {
@@ -64,7 +65,7 @@ class SmartPhone(var load: Double, var batteryPercentage:Int) {
         batteryPercentage = batteryPercentage + chargePercentage
 
             while(batteryPercentage > 100) {
-            batteryPercentage = batteryPercentage - 5
+            batteryPercentage = batteryPercentage - 1
             println(batteryPercentage)
             }
 
@@ -75,4 +76,41 @@ class SmartPhone(var load: Double, var batteryPercentage:Int) {
     }
 
 
+    // send text with load decrement
+    fun sendText(message: String){
+        println(message)
+        load = load - (message.length * 0.02)
+        println("load was deducted per character used in the message. Current load is $load pesos")
+
+    }
+
+
+    fun checkBalance(){
+        if(load > 0 ){
+            println("Your current load is $load")
+
+        }else{
+            println("check operator services.")
+        }
+    }
+
+    fun call(minutes: Int) {
+
+        if (load > 8) {
+
+            var ctr1: Int = 0
+            while ((ctr1 < minutes) && (load > 8)) {
+
+                var costPerMinute: Double = 8.0
+                load = load - costPerMinute
+
+                ctr1++
+            }
+            println("Remaining load: $load.")
+            println("Total call duration: $ctr1 minutes.")
+
+        }else{
+            println("Insufficient Balance.")
+        }
+    }
 }
